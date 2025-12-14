666859b1da46569e782b7960692356a51c249384
sean <playoriginstory@gmail.com>
2025-12-14 11:35:09 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -52,7 +52,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-ancestors https://warpcast.com https://app.farcaster.xyz https://www.base.org https://base.org",
+            value: "frame-ancestors https://warpcast.com https://farcaster.xyz https://app.farcaster.xyz https://www.base.dev https://www.base.org https://base.org",
           },
           {
             key: "X-Frame-Options",
