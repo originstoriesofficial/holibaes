262cf0aed215e8b11c6eedc2a35daea2367846e7
sean <playoriginstory@gmail.com>
2025-12-19 13:49:35 +0100

    fix

--- app/upload-ipfs/route.ts
+++ app/upload-ipfs/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { uploadToIPFS } from '../../lib/ipfs';
+import { uploadToIPFS } from './../lib/ipfs';
 
 export async function POST(req: Request) {
   try {
